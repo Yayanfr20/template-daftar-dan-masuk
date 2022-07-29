8e8f58ef02dce2d1a49d1893120ee114265e10da
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-07-29 14:38:21 +0700

    menambahkan file

---
+++ script.js
@@ -0,0 +1,18 @@
+function masuk() {
+    let formMasuk = document.getElementById('login');
+    let formDaftar = document.getElementById('registrasi');
+
+
+    formDaftar.setAttribute('style', 'display: none;');
+    formMasuk.setAttribute('style', "");
+}
+
+
+function daftar() {
+    let formMasuk = document.getElementById('login');
+    let formDaftar = document.getElementById('registrasi');
+
+
+    formMasuk.setAttribute('style', 'display: none;');
+    formDaftar.setAttribute('style', "");
+}
